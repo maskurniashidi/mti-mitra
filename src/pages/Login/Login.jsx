d7c49abe19664ab1d53bf5c53e7825bf9df7972e
Ilham Santosa <ilham.boys20@gmail.com>
2022-01-13 15:53:14 +0700

    fix: rearrange pages structure Fixes #6

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div>
+      <h1>login</h1>
+    </div>
+  );
+}
+
+export default Login;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 export const APP_ROUTE = [
   {
