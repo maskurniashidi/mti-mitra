e9d3c72cfa6fc9524a7fa61cad76529c733dd9ae
maskurniashidi <maskurshidi12@gmail.com>
2022-01-18 09:23:06 +0700

    rearrange register#02

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 //styling
 import style from "./register.module.css";
 import LogoMti from "../../assets/images/logo.png";
+import reactRouterDom from "react-router-dom";
 
 function Register() {
   return <div className={style.register}></div>;
