a34ac1a2c49c7f5ff2d7771b6ea7a7dabb280ffd
maskurniashidi <maskurshidi12@gmail.com>
2022-01-18 10:49:20 +0700

    finishing register layout

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,47 +1,10 @@
 import React from "react";
-import { Form, Button, Container } from "react-bootstrap";
-import Logo from "../../assets/images/logo.png";
-import FormHeader from "../../assets/images/form-header.png";
-
-import "./Login.scss";
 
 function Login() {
   return (
-    <Container>
-      <div className="login-form">
-        <div className="form-header">
-          <div className="header-background">
-            <img src={FormHeader} />
-          </div>
-          <div className="header-logo">
-            <img src={Logo} />
-          </div>
-        </div>
-        <div className="form-body">
-          <div className="body-title">Welcome Back!</div>
-          <div className="body-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-          <Form>
-            <Form.Floating className="mb-3">
-              <Form.Control id="floatingEmail" type="email" placeholder="name@example.com" />
-              <label htmlFor="floatingEmail">Email address</label>
-            </Form.Floating>
-            <Form.Floating>
-              <Form.Control id="floatingPassword" type="password" placeholder="Password" />
-              <label htmlFor="floatingPassword">Password</label>
-            </Form.Floating>
-            <div className="text-forgot mb-3">
-              <small>Forgot password?</small>
-            </div>
-            <Button className="btn-login mb-4" size="lg">
-              Login
-            </Button>
-            <div className="text-register">
-              Not yet a partner? <a href="#">Register here</a>
-            </div>
-          </Form>
-        </div>
-      </div>
-    </Container>
+    <div>
+      <h1>login</h1>
+    </div>
   );
 }
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,13 +1,41 @@
+//dependency
 import React from "react";
 import { Link } from "react-router-dom";
 
 //styling
 import style from "./register.module.css";
+
+//asset
 import LogoMti from "../../assets/images/logo.png";
-import reactRouterDom from "react-router-dom";
 
 function Register() {
-  return <div className={style.register}></div>;
+  return (
+    <div className={style.register}>
+      <div className={style.registerContainer}>
+        <div className={style.registerHeader}></div>
+        <div className={style.registerLogo}>
+          <img src={LogoMti} alt="logo mti" className={style.logo} />
+        </div>
+        <div className={style.registerText}>
+          <div className={style.textTittle}>Create New Account</div>
+          <div className={style.textDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        </div>
+        <form className={style.registerForm}>
+          <label className={style.registerLabel}>
+            <input type="email" className={style.registerInput} />
+            <span className={style.inputLabel}>Email</span>
+          </label>
+          <button className={style.btnRegister}>Register</button>
+          <p className={style.loginRegister}>
+            Already have an account?<span> </span>
+            <Link className={style.linkToLogin} to="/login">
+              Login now
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
